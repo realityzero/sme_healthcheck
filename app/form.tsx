f91b559ed7e21b34fc3cf776393c08975459c4aa
realityzero <nishantsikri@spit.ac.in>
2023-10-01 14:42:40 +0530

    submit button positioning

--- app/form.tsx
+++ app/form.tsx
@@ -105,7 +105,7 @@ export function ProfileForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mb-5">
         <Card>
             <CardHeader>
                 <CardTitle>Company Information</CardTitle>
@@ -354,7 +354,9 @@ export function ProfileForm() {
             </CardContent>
         </Card>
 
-        <Button type="submit">Submit</Button>
+        <div className="flex ml-auto">
+            <Button type="submit" className="ml-auto">Submit</Button>
+        </div>
       </form>
     </Form>
   )
