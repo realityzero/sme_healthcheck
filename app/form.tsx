e2b3349aaef10f1e4bdf6ea01ee7d04bbd616f0d
realityzero <nishantsikri@spit.ac.in>
2023-10-01 18:58:16 +0530

    fix form validations and ui

--- app/form.tsx
+++ app/form.tsx
@@ -19,11 +19,9 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import FileUploadComponent from "@/components/ui/fileUpload"
 import { FileWithPath } from "react-dropzone"
 import { Checkbox } from "@/components/ui/checkbox"
+import { useState } from "react"
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
   company_uen: z.string().refine((value) => {
     // Check if the length is between 9 and 10 characters
     if (value.length < 9 || value.length > 10) return false;
@@ -68,12 +66,18 @@ const formSchema = z.object({
   }, {
     message: "Enter a 8-digit Mobile Number",
   }),
+  uploadedFiles: z.array(z.string()).refine((files) => {
+    return files.length > 0;
+  }, { message: "Please upload at least one file." }),
+  terms_accept: z.boolean().optional().refine((value) => {
+    console.log('terms_accept', value === true);
+    return value === true;
+  }, {
+    message: 'You must accept the terms and conditions.',
+  }),
 }).refine((check) => check.email === check.re_enter_email, {path: ['re_enter_email'], message: 'Email does not match'});
 
-const handleFileUpload = (uploadedFiles: FileWithPath[]) => {
-    // Handle the uploaded PDF files, e.g., send them to a server or process them.
-    console.log('Uploaded Files:', uploadedFiles);
-  };
+
 
 const uploadTerms = [
     "PDFs (not scanned copies) of company's operating bank current account(s) statements for the past 6 months. Example: If today is 30 Sep 23, then please upload bank statements from Mar 23 to Aug 23 (both months inclusive)",
@@ -93,14 +97,40 @@ export function ProfileForm() {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-          username: "",
         },
-      })
+      });
+
+    const [accepted, setAccepted] = useState(false);
+    const handleAcceptanceChange = (e) => {
+        console.log('handleAcceptanceChange', e.target.checked);
+        setAccepted(e.target.checked);
+    };
+
+    const handleFileUpload = (uploadedFiles: FileWithPath[]) => {
+        // Handle the uploaded PDF files, e.g., send them to a server or process them.
+        console.log('Uploaded Files:', uploadedFiles);
+        const fileNames = uploadedFiles.map((file) => file.name);
     
-    function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // This will be type-safe and validated.
-    console.log(values)
+        // Update the form values with the array of file names
+        form.setValue("uploadedFiles", fileNames);
+    };
+    
+    function onSubmit(form: z.infer<typeof formSchema>) {
+    // function onSubmit(event) {
+        // Do something with the form values.
+        // This will be type-safe and validated.
+        
+        // console.log(form);
+        // event.preventDefault();
+        // const values = form.getValues(); // Get the form values
+        try {
+            // Your validation logic
+            // formSchema.parse(values);
+            console.log('Form submitted:', form);
+        } catch (error) {
+            // Handle validation errors
+            console.error('Validation error:', error);
+        }
     }
 
   return (
@@ -221,7 +251,7 @@ export function ProfileForm() {
                                 <FormItem>
                                 <FormLabel>Mobile Number</FormLabel>
                                 <FormControl>
-                                    <Input type="tel" minLength={10} maxLength={10} defaultValue="+65" {...field} />
+                                    <Input type="tel" minLength={11} maxLength={11} defaultValue="+65" {...field} />
                                 </FormControl>
                                 <FormMessage />
                                 </FormItem>
@@ -236,9 +266,16 @@ export function ProfileForm() {
                 <CardTitle>Upload Documents</CardTitle>
             </CardHeader>
             <CardContent className="flex flex-col justify-around items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-                <div className="w-full md:w-1/2">
+                <div className="flex flex-col items-center space-y-5 w-full md:w-1/2">
                     <FileUploadComponent onUpload={handleFileUpload} />
+                    {form.formState.errors.uploadedFiles && (
+                        <FormMessage >
+                        {form.formState.errors.uploadedFiles.message}
+                        </FormMessage>
+                    )}
                 </div>
+
+
                 <div className="w-full md:w-1/2">
                     {uploadTerms.map((terms, index) => (
                         <div
@@ -297,18 +334,40 @@ export function ProfileForm() {
             <CardContent className="flex flex-col md:flex-row w-full justify-around items-center space-y-4 md:space-x-4 md:space-y-0">
                 <div className="flex flex-col w-full md:w-auto mx-5 space-x-2 space-y-5">
                     <div className="flex flex-row space-x-2">
-                        <Checkbox id="terms1" />
-                        <div className="grid gap-1.5 leading-none">
-                            <label
-                            htmlFor="terms1"
-                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                                Accept terms and conditions
-                            </label>
-                            <p className="text-sm text-muted-foreground">
-                                By ticking, you are confirming that you have understood and are agreeing to the details mentioned:
-                            </p>
-                        </div>
+                        <FormField
+                            control={form.control}
+                            name="terms_accept"
+                            render={({ field }) => (
+                                <FormItem>
+
+                                    <FormControl>
+                                        <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                                        {/* <Checkbox checked={field.value} onCheckedChange={(checked) => {
+                                            console.log("checked:",checked);
+                                            setAccepted(checked as boolean);
+                                            console.log("accepted:",accepted);
+                                            return checked;
+                                        }} /> */}
+                                    </FormControl>
+                                        {form.formState.errors.terms_accept && (
+                                            <FormMessage >
+                                            {form.formState.errors.terms_accept.message}
+                                            </FormMessage>
+                                        )}
+                                    <div className="grid gap-1.5 leading-none">
+                                        <label
+                                        htmlFor="terms1"
+                                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                        >
+                                            Accept terms and conditions
+                                        </label>
+                                        <p className="text-sm text-muted-foreground">
+                                            By ticking, you are confirming that you have understood and are agreeing to the details mentioned:
+                                        </p>
+                                    </div>
+                                </FormItem>
+                            )}
+                        />
                     </div>
                     <div className="w-full md:w-auto">
                         {termsConditions.map((terms, index) => (
