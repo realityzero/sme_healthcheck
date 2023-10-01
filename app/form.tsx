59654ea088026af3313a5a404abd090aee3a7ae3
realityzero <nishantsikri@spit.ac.in>
2023-10-01 12:55:16 +0530

    upload documents section

--- app/form.tsx
+++ app/form.tsx
@@ -16,6 +16,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import FileUploadComponent from "@/components/ui/fileUpload"
+import { FileWithPath } from "react-dropzone"
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -67,6 +69,18 @@ const formSchema = z.object({
   }),
 }).refine((check) => check.email === check.re_enter_email, {path: ['re_enter_email'], message: 'Email does not match'});
 
+const handleFileUpload = (uploadedFiles: FileWithPath[]) => {
+    // Handle the uploaded PDF files, e.g., send them to a server or process them.
+    console.log('Uploaded Files:', uploadedFiles);
+  };
+
+const uploadTerms = [
+    "PDFs (not scanned copies) of company's operating bank current account(s) statements for the past 6 months. Example: If today is 30 Sep 23, then please upload bank statements from Mar 23 to Aug 23 (both months inclusive)",
+    "If your company is multi-banked, then please upload 6 months bank statements for each bank account",
+    "If your file is password protected, we request you to remove the password and upload the file to avoid submission failure",
+    "In case if you are facing any issue while uploading bank statements, Please contact us on support@credilinq.ai",
+];
+
 export function ProfileForm() {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -77,7 +91,7 @@ export function ProfileForm() {
     
     function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    // This will be type-safe and validated.
     console.log(values)
     }
 
@@ -209,7 +223,65 @@ export function ProfileForm() {
                 </div>
             </CardContent>
         </Card>
-        
+        <Card>
+            <CardHeader>
+                <CardTitle>Upload Documents</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col justify-around items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+                <div className="w-full md:w-1/2">
+                    <FileUploadComponent onUpload={handleFileUpload} />
+                </div>
+                <div className="w-full md:w-1/2">
+                    {uploadTerms.map((terms, index) => (
+                        <div
+                        key={index}
+                        className="mb-2 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0 space-x-5"
+                        >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 24 24"
+                            className="mx-auto text-2xl rounded-full bg-opacity-12"
+                        >
+                            <path d="M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path>
+                        </svg>
+                        <div className="space-y-1">
+                        <p className="text-sm font-light leading-7 tracking-wide">
+                            {terms.includes('support@credilinq.ai') ? (
+                                terms.split('support@credilinq.ai').map((text, index) => (
+                                    index === 0 ? (
+                                        <span key={index}>{text}</span>
+                                    ) : (
+                                        <a
+                                            key={index}
+                                            href={`mailto:support@credilinq.ai`}
+                                            onMouseOver={(e) => {
+                                                if (e.target instanceof HTMLAnchorElement) {
+                                                    e.target.href = `mailto:support@credilinq.ai`
+                                                }
+                                            }}
+                                            onMouseOut={(e) => {
+                                                if (e.target instanceof HTMLAnchorElement) {
+                                                    e.target.href = '#'
+                                                }
+                                            }}
+                                            style={{color: "rgb(96, 26, 121)"}}
+                                        >
+                                            support@credilinq.ai
+                                        </a>
+                                    )
+                                ))
+                            ) : (
+                                terms
+                            )}
+                        </p>
+                        </div>
+                        </div>
+                    ))}
+                </div>
+            </CardContent>
+        </Card>
 
         <Button type="submit">Submit</Button>
       </form>
